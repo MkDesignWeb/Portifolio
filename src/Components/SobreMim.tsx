d229132b82200c9b2633511b6c206118a24627b2
MkDesignWeb <99919855+MkDesignWeb@users.noreply.github.com>
2022-09-10 17:49:25 -0300

    Char MK 3d

--- src/Components/SobreMim.tsx
+++ src/Components/SobreMim.tsx
@@ -1,13 +1,45 @@
-export function SobreMim(){
-    return(
+import { PresentationControls } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
+
+import { CharMKW } from "../Assets/Models/CharMKW"
+
+export function SobreMim() {
+    return (
         <div className="text-white">
             <h1 className="font-Roboto-Medium text-3xl">Sobre <span className="text-primary">mim</span></h1>
             <p className="font-Roboto-Light text-sm">Veja um pouco sobre mim</p>
 
-            <div className="mt-8 max-w-[280px]">
-                <p className="text-sm">
-                Trabalho com Designer gráfico e desenvolvimento de sites para web com foco na biblioteca ReactJs para criar sites performáticos e com a mais nova tecnologia do mercado. Tenho muita persistência em aprender novas áreas que tenham um grande impacto visual no cotidiano de todos dentro ou fora da web.
-                </p>
+            <div className="flex flex-col md:flex-row  gap-10">
+                <div className="mt-8 md:max-w-[40vw]">
+                    <p className="text-sm">
+                        Trabalho com Designer gráfico e desenvolvimento de sites para web com foco na biblioteca ReactJs para criar sites performáticos e com a mais nova tecnologia do mercado. Tenho muita persistência em aprender novas áreas que tenham um grande impacto visual no cotidiano de todos dentro ou fora da web.
+                    </p>
+                </div>
+
+                <div className="w-screen flex justify-center h-80 relative">
+                    <div className="absolute w-full md:h-96 h-72 md:-top-32 ">
+                        <Canvas
+                            className="touch-none"
+                            shadows
+                            flat
+                            linear
+                        >
+                            <ambientLight intensity={0.7} />
+                            <pointLight position={[10,10,10]} intensity={2.4}/>
+
+                            <PresentationControls
+                                global
+                                config={{ mass: 2, tension: 500 }}
+                                snap={{ mass: 4, tension: 1500 }}
+                                polar={[-Math.PI / 3, Math.PI / 3]}
+                                azimuth={[-Math.PI / 1.4, Math.PI / 2]}>
+
+                                <CharMKW scale={1.7} rotation={[0.2, -0.8, 0]} position={[-0.3, -1, 0]} />
+                            </PresentationControls>
+
+                        </Canvas>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/Pages/PDev.tsx
+++ src/Pages/PDev.tsx
@@ -10,26 +10,28 @@ import { MainBotton } from '../Components/MainBotton';
 import { FaUser } from 'react-icons/fa';
 import { SobreMim } from '../Components/SobreMim';
 
-export function PDev(){
+export function PDev() {
 
-    const {width} = useWindowDimensions()
+    const { width } = useWindowDimensions()
 
-    return(
+    return (
         <section className='max-w-screen overflow-x-hidden bg-fundo'>
             <img src={brilhoMobile} alt="" className='absolute top-0 right-0 z-10 w-full' />
-            <div className="Header w-full flex justify-center items-center z-20 relative">
-            <Header desc="Desenvolvedor Front-END focado em React JS" logo={LogoMKW}/>
+            <div className="w-full h-auto flex justify-center items-center z-20 relative">
+                <Header desc="Desenvolvedor Front-END focado em React JS" logo={LogoMKW} />
             </div>
 
-            <div className="Sobre-mim mt-16 h-[7000px] w-full relative z-20">
-            <nav className='nav-botton w-full flex flex-wrap justify-center gap-2 z-30 relative'>
-                <MainBotton name='Sobre mim' icon={<FaUser />}/>
-                <MainBotton name='Sobre mim' icon={<FaUser />}/>
-                <MainBotton name='Sobre mim' icon={<FaUser />}/>
-            </nav>
-                <img src={width > 786 ? CurvePc : Curve} alt="" className="h-32 w-full object-cover object-top absolute top-5 lef-0 z-10"/>
-                <div className='bg-black-scudary relative top-14 w-full h-[300px]'>
-                    <div className='relative z-20 px-7'>
+            <div className="h-auto w-full relative z-20">
+
+                <nav className='w-full flex flex-wrap justify-center  z-30 relative top-9'>
+                    <MainBotton name='Sobre mim' icon={<FaUser />} />
+                    <MainBotton name='Sobre mim' icon={<FaUser />} />
+                    <MainBotton name='Sobre mim' icon={<FaUser />} />
+                </nav>
+                <img src={width > 786 ? CurvePc : Curve} alt="" className="h-32 w-full object-cover object-top top-5 lef-0 z-10" />
+
+                <div className='bg-black-scudary relative  w-full h-auto'>
+                    <div className=' mx-auto max-w-[1728px] relative z-20 px-7 md:px-28'>
                         <SobreMim />
                     </div>
                 </div>
