9f66c2e2aa712f4b1a3cbfc4f77c2c9c4c0b65d2
MkDesignWeb <99919855+MkDesignWeb@users.noreply.github.com>
2023-01-03 11:18:30 -0300

    Modal Designer

--- src/Components/TrabalhosDes.tsx
+++ src/Components/TrabalhosDes.tsx
@@ -1,4 +1,29 @@
+import { gql, useQuery } from "@apollo/client"
+import { useState } from "react"
+import { MdOutlineClose } from "react-icons/md"
+
+const GET_IMG = gql`
+query {
+    assets {
+        url
+      }
+    }
+    
+`
+
+interface AssetsImg {
+    assets: {
+        url: string
+      }[]
+    }
+
+
 export function TrabalhosDes(){
+
+    const { data } = useQuery<AssetsImg>(GET_IMG) 
+   
+    const [UrlModal, setUrlMoal] = useState('')
+
     return(
         <section className="mt-20 mb-32 relative">
             <div className='w-full flex flex-col items-center text-white mb-10'>
@@ -6,27 +31,38 @@ export function TrabalhosDes(){
                 <p className="font-Roboto-Light text-sm text-center max-w-xs">Veja aqui meus trabalhos de criação e edição de imagens</p>
             </div>
 
-            <div className="text-white">
+          
                 <div className="mx-auto max-w-6xl flex flex-wrap justify-center ">
-                    <img src="" alt="" className="bg-red-600 w-40 h-40 sm:w-52 sm:h-52" />
-                    <img src="" alt="" className="bg-red-600 w-40 h-40 sm:w-52 sm:h-52" />
-                    <img src="" alt="" className="bg-red-600 w-40 h-40 sm:w-52 sm:h-52" />
-                    <img src="" alt="" className="bg-red-600 w-40 h-40 sm:w-52 sm:h-52" />
-                    <img src="" alt="" className="bg-red-600 w-40 h-40 sm:w-52 sm:h-52" />
-                    <img src="" alt="" className="bg-red-600 w-40 h-40 sm:w-52 sm:h-52" />
-                    <img src="" alt="" className="bg-red-600 w-40 h-40 sm:w-52 sm:h-52" />
-                    <img src="" alt="" className="bg-red-600 w-40 h-40 sm:w-52 sm:h-52" />
-                    <img src="" alt="" className="bg-red-600 w-40 h-40 sm:w-52 sm:h-52" />
-                    <img src="" alt="" className="bg-red-600 w-40 h-40 sm:w-52 sm:h-52" />
-                    <img src="" alt="" className="bg-red-600 w-40 h-40 sm:w-52 sm:h-52" />
-                    <img src="" alt="" className="bg-red-600 w-40 h-40 sm:w-52 sm:h-52" />
-                    <img src="" alt="" className="bg-red-600 w-40 h-40 sm:w-52 sm:h-52" />
-                    <img src="" alt="" className="bg-red-600 w-40 h-40 sm:w-52 sm:h-52" />
-                   
-
+                    {data?.assets.map((data) => {
+                        return(
+                        <div className="md:hover:scale-105 md:hover:z-30 relative after:z-30 after:content-[''] after:absolute after:w-[calc(100%+1rem)] after:h-[calc(100%+1rem)] after:transition-all after:rounded-xl after:border-primary after:-top-2 after:-left-2 after:pointer-events-none  md:after:hover:border-2  transition-all"
+                        
+                        onClick={() => {setUrlMoal(data.url)}}
+                        >
+                            <img src={data.url} alt="" className="bg-red-600 w-40 h-40 sm:w-52 sm:h-52 object-cover" />
+                        </div>
+                        )
+                    })}
                 </div>
-            </div>
-            
+                
+                {UrlModal ? (
+                   <div className="w-screen h-screen fixed z-40 top-0 left-0 flex justify-center items-center">
+      
+                   <div className="relative z-40
+                   ">
+                   
+                   <div className='absolute -top-10 right-0 z-40'> <MdOutlineClose size={35} color='#fff' onClick={() => setUrlMoal('')}/> </div>
+           
+                   <img src={UrlModal} alt="" className="max-w-[95vw] max-h-[80vh] md:object-cover"/>
+                   
+           
+                   </div>
+                   <div className="w-screen h-screen bg-[#000000ef] fixed top-0 left-0 z-10 " onClick={() => props.Modal()}/>
+                   </div>
+         
+                ): ''}
+                
+
         </section>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
+import { ApolloProvider } from '@apollo/client'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { client } from './Lib/Apollo'
 
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>
 )
